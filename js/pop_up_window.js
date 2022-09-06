9cca06820baff69495cb1ab18ac399ba11fb6712
Haider Ahmad <hidarahmadjob@gmail.com>
2022-09-06 20:16:51 +0430

    Project section added Dynamicly using Javascript

---
+++ js/pop_up_window.js
@@ -0,0 +1,152 @@
+// Project Information
+const projectesData = [
+  {
+    id: 1,
+    name: "Multi-Post Stories Gain+Glory",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque at voluptate dolore eos a commodi ratione distinctio tenetur, velit reprehenderit sit, voluptatem odio atque hic eveniet eaque esse sunt nesciunt sed libero similique. Expedita, asperiores. Harum, nemo! Eum ipsum est hic! Explicabo fugiat, deserunt maiores quasi nesciunt accusamus in. Temporibus et dolor, ea fuga illum consectetur recusandae iste blanditiis saepe excepturi maiores deserunt consequatur quidem laborum. Similique expedita quod impedit autem doloribus, minus inventore provident modi molestias placeat dolores recusandae excepturi accusamus aliquam laboriosam magni? Maiores aliquid eos tempore, dignissimos fuga porro quae natus repudiandae non ullam dicta officia qui.",
+    featured_image: "images/Img-Plaholder.svg",
+    technologies: "Ruby on rails,CSS,Javascript,HTML",
+    link_to_live_version: "https://hidar37.github.io/",
+    link_to_sourse: "https://github.com/Hidar37/Hidar37.github.io"
+  },
+  {
+    id: 2,
+    name: "Multi-Post Stories Gain+Glory",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque at voluptate dolore eos a commodi ratione distinctio tenetur, velit reprehenderit sit, voluptatem odio atque hic eveniet eaque esse sunt nesciunt sed libero similique. Expedita, asperiores. Harum, nemo! Eum ipsum est hic! Explicabo fugiat, deserunt maiores quasi nesciunt accusamus in. Temporibus et dolor, ea fuga illum consectetur recusandae iste blanditiis saepe excepturi maiores deserunt consequatur quidem laborum. Similique expedita quod impedit autem doloribus, minus inventore provident modi molestias placeat dolores recusandae excepturi accusamus aliquam laboriosam magni? Maiores aliquid eos tempore, dignissimos fuga porro quae natus repudiandae non ullam dicta officia qui.",
+    featured_image: "images/Img-Plaholder.svg",
+    technologies: "Ruby on rails,CSS,Javascript,HTML",
+    link_to_live_version: "https://hidar37.github.io/",
+    link_to_sourse: "https://github.com/Hidar37/Hidar37.github.io"
+  },
+  {
+    id: 3,
+    name: "Multi-Post Stories Gain+Glory",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque at voluptate dolore eos a commodi ratione distinctio tenetur, velit reprehenderit sit, voluptatem odio atque hic eveniet eaque esse sunt nesciunt sed libero similique. Expedita, asperiores. Harum, nemo! Eum ipsum est hic! Explicabo fugiat, deserunt maiores quasi nesciunt accusamus in. Temporibus et dolor, ea fuga illum consectetur recusandae iste blanditiis saepe excepturi maiores deserunt consequatur quidem laborum. Similique expedita quod impedit autem doloribus, minus inventore provident modi molestias placeat dolores recusandae excepturi accusamus aliquam laboriosam magni? Maiores aliquid eos tempore, dignissimos fuga porro quae natus repudiandae non ullam dicta officia qui.",
+    featured_image: "images/Img-Plaholder.svg",
+    technologies: "Ruby on rails,CSS,Javascript,HTML",
+    link_to_live_version: "https://hidar37.github.io/",
+    link_to_sourse: "https://github.com/Hidar37/Hidar37.github.io"
+  },
+  {
+    id: 4,
+    name: "Multi-Post Stories Gain+Glory",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque at voluptate dolore eos a commodi ratione distinctio tenetur, velit reprehenderit sit, voluptatem odio atque hic eveniet eaque esse sunt nesciunt sed libero similique. Expedita, asperiores. Harum, nemo! Eum ipsum est hic! Explicabo fugiat, deserunt maiores quasi nesciunt accusamus in. Temporibus et dolor, ea fuga illum consectetur recusandae iste blanditiis saepe excepturi maiores deserunt consequatur quidem laborum. Similique expedita quod impedit autem doloribus, minus inventore provident modi molestias placeat dolores recusandae excepturi accusamus aliquam laboriosam magni? Maiores aliquid eos tempore, dignissimos fuga porro quae natus repudiandae non ullam dicta officia qui.",
+    featured_image: "images/Img-Plaholder.svg",
+    technologies: "Ruby on rails,CSS,Javascript,HTML",
+    link_to_live_version: "https://hidar37.github.io/",
+    link_to_sourse: "https://github.com/Hidar37/Hidar37.github.io"
+  },
+  {
+    id: 5,
+    name: "Multi-Post Stories Gain+Glory",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque at voluptate dolore eos a commodi ratione distinctio tenetur, velit reprehenderit sit, voluptatem odio atque hic eveniet eaque esse sunt nesciunt sed libero similique. Expedita, asperiores. Harum, nemo! Eum ipsum est hic! Explicabo fugiat, deserunt maiores quasi nesciunt accusamus in. Temporibus et dolor, ea fuga illum consectetur recusandae iste blanditiis saepe excepturi maiores deserunt consequatur quidem laborum. Similique expedita quod impedit autem doloribus, minus inventore provident modi molestias placeat dolores recusandae excepturi accusamus aliquam laboriosam magni? Maiores aliquid eos tempore, dignissimos fuga porro quae natus repudiandae non ullam dicta officia qui.",
+    featured_image: "images/Img-Plaholder.svg",
+    technologies: "Ruby on rails,CSS,Javascript,HTML",
+    link_to_live_version: "https://hidar37.github.io/",
+    link_to_sourse: "https://github.com/Hidar37/Hidar37.github.io"
+  },
+  {
+    id: 6,
+    name: "Multi-Post Stories Gain+Glory",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque at voluptate dolore eos a commodi ratione distinctio tenetur, velit reprehenderit sit, voluptatem odio atque hic eveniet eaque esse sunt nesciunt sed libero similique. Expedita, asperiores. Harum, nemo! Eum ipsum est hic! Explicabo fugiat, deserunt maiores quasi nesciunt accusamus in. Temporibus et dolor, ea fuga illum consectetur recusandae iste blanditiis saepe excepturi maiores deserunt consequatur quidem laborum. Similique expedita quod impedit autem doloribus, minus inventore provident modi molestias placeat dolores recusandae excepturi accusamus aliquam laboriosam magni? Maiores aliquid eos tempore, dignissimos fuga porro quae natus repudiandae non ullam dicta officia qui.",
+    featured_image: "images/Img-Plaholder.svg",
+    technologies: "Ruby on rails,CSS,Javascript,HTML",
+    link_to_live_version: "https://hidar37.github.io/",
+    link_to_sourse: "https://github.com/Hidar37/Hidar37.github.io"
+  }
+]
+
+// Main Container Address
+const project_section = document.getElementById("move-top");
+//  h1
+let add_h1 = document.createElement('h1');
+add_h1.setAttribute('class', 'recent-work');
+add_h1.appendChild(document.createTextNode('My Recent Works'));
+project_section.appendChild(add_h1);
+
+// Card contaienr
+let cardContainer = document.createElement('div');
+cardContainer.setAttribute('class', 'card-container');
+project_section.appendChild(cardContainer);
+
+// Adding the entire information
+for (var x = 0; x < projectesData.length; x++) {
+  // Card Container -> Card-1
+  let card = document.createElement('div');
+  card.setAttribute('class', 'card');
+  card.setAttribute('data-project-id', projectesData[x].id);
+  cardContainer.appendChild(card);
+
+  // Card-1 -> child 4/1
+  let card_img_div = document.createElement('div');
+  card_img_div.setAttribute('class', 'card-img');
+  // Card-1 -> child 4/2
+  let card_title_div = document.createElement('div');
+  card_title_div.setAttribute('class', 'card-title');
+  // Card-1 -> child 4/3
+  let card_desc_div = document.createElement('div');
+  card_desc_div.setAttribute('class', 'card-desc');
+  // Card-1 -> child 4/4
+  let card_btn_div = document.createElement('div');
+  card_btn_div.setAttribute('class', 'card-btn');
+
+  // Appending the child-div to parent(card)
+  card.appendChild(card_img_div);
+  card.appendChild(card_title_div);
+  card.appendChild(card_desc_div);
+  card.appendChild(card_btn_div);
+  // card -> child 4/1 -> card-title -> childs
+  // adding a tag
+  let card_img_a_tag = document.createElement('a');
+  card_img_a_tag.setAttribute('href', '#');
+  card_img_a_tag.setAttribute('class', 'img-card-link');
+  card_img_div.appendChild(card_img_a_tag);
+  // adding img to a tag
+  let card_img_tag = document.createElement('img');
+  card_img_tag.setAttribute('src', projectesData[x].featured_image);
+  card_img_tag.setAttribute('alt', 'Project Image');
+  card_img_a_tag.appendChild(card_img_tag);
+
+  // card -> child 4/2 -> card-title -> childs
+  // adding p tag
+  let card_title_p_tag = document.createElement('p');
+  card_title_div.appendChild(card_title_p_tag);
+  // adding a tag
+  let card_title_a_tag = document.createElement('a');
+  card_title_a_tag.setAttribute('href', '#');
+  card_title_a_tag.setAttribute('class', 'card-title-link');
+  card_title_a_tag.appendChild(document.createTextNode(projectesData[x].name));
+  card_title_p_tag.appendChild(card_title_a_tag);
+
+  // card -> child 4/3 -> card-desc -> childs
+  // add ul
+  let card_desc_ul = document.createElement('ul');
+  card_desc_div.appendChild(card_desc_ul);
+
+  // split the languages in order to find the lenght and show each technologies used in the project.
+  let countTechnologies = projectesData[x].technologies.split(",");
+  for (var i = 0; i < countTechnologies.length; i++) {
+    // add li
+    let card_desc_li = document.createElement('li');
+    card_desc_ul.appendChild(card_desc_li);
+    // adding a
+    let card_desc_a = document.createElement('a');
+    card_desc_a.setAttribute('href', '#');
+    card_desc_a.appendChild(document.createTextNode(countTechnologies[i]));
+    card_desc_li.appendChild(card_desc_a);
+  }
+
+  // card -> child 4/4 -> card-btn -> childs
+  // adding p tag
+  let card_btn_p_tag = document.createElement('p');
+  card_btn_div.appendChild(card_btn_p_tag);
+  // adding a tag
+  let card_btn_a_tag = document.createElement('button');
+  card_btn_a_tag.setAttribute('class', 'btn-link');
+  card_btn_a_tag.setAttribute('id', 'see-project');
+  card_btn_a_tag.setAttribute('data-modal-target', '#modal');
+  card_btn_a_tag.appendChild(document.createTextNode('See Project'));
+  card_btn_p_tag.appendChild(card_btn_a_tag);
+}
+
+// =======================================
