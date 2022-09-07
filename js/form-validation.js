61f978def60948c6513e4cc42696eba5975af008
Haider Ahmad <hidarahmadjob@gmail.com>
2022-09-08 00:03:18 +0430

    JS linter added. js linter error fixed. type error removed.

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,15 +1,15 @@
-document.getElementById("form").addEventListener('submit', (event) => {
-  let emailField = document.getElementById("email").value;
+/* eslint-disable linebreak-style */
+document.getElementById('form').addEventListener('submit', (event) => {
+  const emailField = document.getElementById('email').value;
   if (emailField.toLowerCase() === emailField) {
     return true;
   }
-  else {
-    var myForm = document.getElementById("form");
-    var addP = document.createElement("p");
-    addP.setAttribute('class', 'error-message');
-    addP.appendChild(document.createTextNode("Submition Faild: Pleas type your email in lowercase."));
-    myForm.appendChild(addP);
-    event.preventDefault();
-    return false;
-  }
+
+  const myForm = document.getElementById('form');
+  const addP = document.createElement('p');
+  addP.setAttribute('class', 'error-message');
+  addP.appendChild(document.createTextNode('Submition Faild: Please type your email in lowercase.'));
+  myForm.appendChild(addP);
+  event.preventDefault();
+  return false;
 });
--- js/hamburger_effect.js
+++ js/hamburger_effect.js
@@ -1,30 +1,11 @@
-// Hide all the page section
-function hideAllSection() {
-  document.getElementById("move-top").style.display = "none";
-  document.getElementById("about-section").style.display = "none";
-  document.getElementById("soft-skill").style.display = "none";
-  document.getElementById("contact-section").style.display = "none";
-  document.getElementById("footer").style.display = "none";
-}
-// Show all the Page Section
-function showAllSection() {
-  document.getElementById("move-top").style.display = "block";
-  document.getElementById("about-section").style.display = "block";
-  document.getElementById("soft-skill").style.display = "block";
-  document.getElementById("contact-section").style.display = "block";
-  document.getElementById("footer").style.display = "block";
-}
-
 // when click on 3bar icon (Hide all the page excpet Humbarger Menu)
-var showHideMenu = document.getElementById("humbarger-mobile");
-showHideMenu.addEventListener("click", function () {
-  document.getElementById("mobile-nav").style.transform = "translateX(0%)";
-  // hideAllSection();
+const showHideMenu = document.getElementById('humbarger-mobile');
+showHideMenu.addEventListener('click', () => {
+  document.getElementById('mobile-nav').style.transform = 'translateX(0%)';
 });
 
 // when click on X icon (Hide Humbarger Menu and show all the Page)
-document.getElementById("humbarger").addEventListener("click", function () {
-  document.getElementById("mobile-nav").style.transition = "all 1s";
-  document.getElementById("mobile-nav").style.transform = "translateX(100%)";
-  // showAllSection();
+document.getElementById('humbarger').addEventListener('click', () => {
+  document.getElementById('mobile-nav').style.transition = 'all 1s';
+  document.getElementById('mobile-nav').style.transform = 'translateX(100%)';
 });
